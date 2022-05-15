84a9b98ec34b47e5356d8e2e7587ea2186fb3453
Sergey Chipyshev <pochta1943@gmail.com>
2022-05-15 14:35:53 +0500

    complite homework 08-ansible-03-yandex

---
+++ src/terraform/ansible.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "wait" {
+  provisioner "local-exec" {
+    command = "sleep 120"
+  }
+
+  depends_on = [
+    local_file.inventory
+  ]
+}
+
+resource "null_resource" "site" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory ../ansible/site.yml --diff"
+  }
+
+  depends_on = [
+    null_resource.wait
+  ]
+}
---
+++ src/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "yandex_cloud_id" {
+  default = "b1gffcps5oa5h9clc5o9"
+}
+
+variable "yandex_folder_id" {
+  default = "b1g27gpcstr1l1bi3a22"
+}
+
+variable "centos-7" {
+  default = "fd8p7vi5c5bbs2s5i67s"
+}
